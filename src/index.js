90bad27d630764a0f3bf19f30bfb61eb4520c7c5
Sagar Gohil <sagargohil344@gmail.com>
2021-03-19 21:10:07 +0530

    version 2

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-function redirect(url){
-    location.replace(url)
-}
 
-function downloadCV(){
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('#photo').style.width = "100%";
+  document.querySelector('#photo').style.height = "100%";
+});
 
-}
-
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // Get the forms we want to add validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
+// (function() {
+//     'use strict';
+//     window.addEventListener('load', function() {
+//       // Get the forms we want to add validation styles to
+//       var forms = document.getElementsByClassName('needs-validation');
+//       // Loop over them and prevent submission
+//       var validation = Array.prototype.filter.call(forms, function(form) {
+//         form.addEventListener('submit', function(event) {
+//           if (form.checkValidity() === false) {
+//             event.preventDefault();
+//             event.stopPropagation();
+//           }
+//           form.classList.add('was-validated');
+//         }, false);
+//       });
+//     }, false);
+//   })();
